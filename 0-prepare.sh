084e1b6dd3555f249ac34813d15881068f35c644
pietervincken <4153479+pietervincken@users.noreply.github.com>
2022-02-14 17:43:15 +0100

    minor improvements

--- 0-prepare.sh
+++ 0-prepare.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+if [ -z $subscription ]; then
+    echo "Could not find subscription. Stopping!"
+    exit 1
+fi
+
+if [ -z $rgstate ]; then
+    echo "Could not find rgstate. Stopping!"
+    exit 1
+fi
+
+if [ -z $location ]; then
+    echo "Could not find location. Stopping!"
+    exit 1
+fi
+
+if [ -z $sastate ]; then
+    echo "Could not find sastate. Stopping!"
+    exit 1
+fi
+
 az account set -s $subscription
 az group show --name $rgstate || az group create -l $location -n $rgstate
 az storage account show --name $sastate || (az storage account create -n $sastate -g $rgstate -l $location --sku Standard_LRS && az storage container create -n tfstate --account-name $sastate --public-access blob --auth-mode login)
